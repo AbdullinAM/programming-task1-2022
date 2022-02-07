9495999e5c48be6ce223f524e847c77cfd2eb214
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2022-02-07 14:27:31 +0300

    init

--- app/src/test/java/programming/task1/TestMyTask.java
+++ app/src/test/java/programming/task1/TestMyTask.java
@@ -6,6 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestMyTask {
     @Test public void foo() {
-        assertEquals(42, new MyTask().foo());
+        assertEquals(44, new MyTask().foo());
     }
 }
